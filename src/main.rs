df189d7884c3544a0777194052d589ee8132a1d2
werdl <werdl_is_cool@outlook.com>
2024-02-11 10:51:35 +0000

    cool

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
-use std::io::stdin;
+use std::{io::stdin, option, time::Duration};
 
 use reqwest::{header, Response};
 use serde_json;
 
 use git2::Repository;
+
+use clap::{arg, Parser};
+
+use indicatif::{ProgressBar, ProgressStyle};
 
 async fn fetch_repo_page(
     user: String,
@@ -15,18 +19,27 @@ async fn fetch_repo_page(
         user, page
     );
 
-    println!("Fetching {}", url);
-    let client = reqwest::Client::new();
+    let pb = ProgressBar::new_spinner();
+    pb.set_style(ProgressStyle::default_spinner().progress_chars("⣾⣽⣻⢿⡿⣟⣯⣷"));
+    pb.set_message("Fetching repos...");
+
+    pb.enable_steady_tick(Duration::from_millis(25));
+
     let response = reqwest::Client::new()
         .get(&url)
         .header("User-Agent", "werdl/gh-clone-all")
-        // .header(
-        //     "Authorization",
-        //     header::HeaderValue::from_str(&auth_token).unwrap(),
-        // )
-        .header("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8")
+        .header(
+            "Authorization",
+            header::HeaderValue::from_str(&auth_token).unwrap(),
+        )
+        .header(
+            "Accept",
+            "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+        )
         .send()
         .await?;
+
+    pb.finish_with_message("Fetched repos");
 
     Ok(response)
 }
@@ -38,7 +51,14 @@ async fn fetch_user_repos(
     let mut page = 1;
     let mut repos = Vec::new();
     let mut http_urls = Vec::new();
+
+    let pb = ProgressBar::new_spinner();
+
+    pb.set_style(ProgressStyle::default_spinner().progress_chars("⣾⣽⣻⢿⡿⣟⣯⣷"));
+    pb.set_message("Fetching repos (page 1)...");
+
     loop {
+        pb.set_message(format!("Fetching repos (page {})...", page));
         let response = fetch_repo_page(user.clone(), page, auth_token.clone()).await?;
 
         // check if the request was successful
@@ -67,12 +87,11 @@ async fn fetch_user_repos(
         http_urls.extend(http_urls_on_page);
 
         // check if there is a next page
-        if let Some(link_header) = fetch_repo_page(user.clone(), page, auth_token.clone())
+        if let Some(_) = fetch_repo_page(user.clone(), page, auth_token.clone())
             .await?
             .headers()
             .get(header::LINK)
         {
-            let link_header = link_header.to_str().unwrap();
             // increment the page number and continue fetching
             page += 1;
             continue;
@@ -81,39 +100,63 @@ async fn fetch_user_repos(
         // break the loop if there is no next page
         break;
     }
-    Ok((repos
+
+    pb.finish_with_message("Fetched repos");
+
+    Ok(repos
         .into_iter()
         .zip(http_urls.into_iter())
-        .collect::<Vec<(String, String)>>()))
+        .collect::<Vec<(String, String)>>())
 }
 
-async fn clone_repos(repos: Vec<(String, String)>, path_prefix: String) -> Result<(), reqwest::Error> {
+async fn clone_repos(
+    repos: Vec<(String, String)>,
+    path_prefix: String,
+) -> Result<(), reqwest::Error> {
+    let pb = ProgressBar::new(repos.len() as u64);
     for (repo, http_url) in repos {
-        println!("Cloning {} from {}", repo, http_url);
+        pb.inc(1);
+        pb.set_message(format!("Cloning {}...", repo));
         let repo = match Repository::clone(&http_url, format!("{}/{}", path_prefix, repo)) {
             Ok(repo) => repo,
             Err(e) => panic!("failed to clone: {}", e),
         };
-
-        println!("Cloned {} to {:?}", http_url, repo.path());
     }
+
+    pb.finish_with_message("Cloned repos");
     Ok(())
+}
+
+#[derive(Parser, Debug)]
+struct Options {
+    #[arg(short, long, default_value = "werdl")]
+    user: String,
+
+    #[arg(short, long, default_value = "./")]
+    path_prefix: String,
+
+    #[arg(short, long, default_value = "")]
+    auth_token: String,
+
+    #[arg(short, long, default_value = "false")]
+    clone: bool,
 }
 
 #[tokio::main]
 async fn main() -> Result<(), reqwest::Error> {
-    println!("Please enter your GitHub token:");
-    let mut auth_token = String::new();
-    let _ = stdin().read_line(&mut auth_token).unwrap();
-
-    auth_token = auth_token.trim().to_string();
+    let options = Options::parse();
 
     let user = "werdl".to_string();
-    let repos = fetch_user_repos(user.clone(), auth_token).await?;
+    let repos = fetch_user_repos(options.user.clone(), options.auth_token).await?;
     for repo in repos.clone() {
         println!("{:?}", repo);
     }
 
-    println!("Found in total {} repos for {}", repos.len(), user);
+    println!("Found in total {} repos for {}", repos.len(), options.user);
+
+    if options.clone {
+        clone_repos(repos, options.path_prefix).await?;
+    }
+
     Ok(())
 }
